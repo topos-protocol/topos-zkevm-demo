5ccb21c6d4010ebc8b24db35df0fc5735a47debe
Monir Hadji <hadji.szs@gmail.com>
2024-01-24 09:26:00 +0100

    chore: add logo and fix typo

--- src/commands/init.command.ts
+++ src/commands/init.command.ts
@@ -125,7 +125,7 @@ export class InitCommand extends CommandRunner {
           }
         },
         error: () => {
-          logError(`❌ Git Not Intalled!`)
+          logError(`❌ Git Not Installed!`)
         },
       })
     )
