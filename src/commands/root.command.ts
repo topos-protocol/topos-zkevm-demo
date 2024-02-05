f81eac04b0d5c1892dcd88f0cb1f3061daa68470
Sébastien Dan <sebastien.dan@gmail.com>
2024-02-05 14:40:06 +0100

    fix: update root cmd examples

--- src/commands/root.command.ts
+++ src/commands/root.command.ts
@@ -14,26 +14,46 @@ const example = chalk.green
 const helptext = `
 Example Usage
 
-  $ ${example('topos-zkevm-demo init')}
-    Initialize the Topos-zkEVM-Demo. This command will output the status of the topos zkevm demo creation to the terminal as it runs, and will log a more detailed status to a log file.
+  $ ${example('topos-zkevm-demo install')}
+    Install Topos-zkEVM-Demo. This command will output the status of the topos zkevm demo creation to the terminal as it runs, and will log a more detailed status to a log file.
+
+  $ ${example('topos-zkevm-demo start')}
+    Start the local-zkEVM infra to get your development chain up and running.
 
   $ ${example('topos-zkevm-demo execute')}
     Execute the Topos-zkEVM-Demo demo script to deploy contracts and execute transactions.
 
-  $ ${example('topos-zkevm-demo get-receipt')}
-    Get a transaction receipt from a transaction hash.
+  $ ${example(
+    'topos-zkevm-demo generate merkle-proof 0x064eaad7fb35c25410aec7f55603d9bb95436891cbda68a4be9a6fc3b19ca0a5'
+  )}
+    Generate a receipt merkle proof for the 0x064eaad7fb35c25410aec7f55603d9bb95436891cbda68a4be9a6fc3b19ca0a5 transaction.
 
-  $ ${example('topos-zkevm-demo get-receipt-trie-root')}
-    Get a receipt trie root from a block number.
+  $ ${example('topos-zkevm-demo generate zk-proof 2')}
+    Generate a zk-proof for block 2.
 
-  $ ${example('topos-zkevm-demo clean')}
-    This will clean the topos zkevm demo. It will shut down all containers, and remove all filesystem artifacts except for log files.
+  $ ${example(
+    'topos-zkevm-demo util get-receipt-trie-root 0x064eaad7fb35c25410aec7f55603d9bb95436891cbda68a4be9a6fc3b19ca0a5'
+  )}
+    Get the receipt trie root of the block of the 0x064eaad7fb35c25410aec7f55603d9bb95436891cbda68a4be9a6fc3b19ca0a5 transaction.
+
+  $ ${example(
+    'topos-zkevm-demo verify merkle-proof 0x064eaad7fb35c25410aec7f55603d9bb95436891cbda68a4be9a6fc3b19ca0a5 0xf851a02d247ca1770e3221e8aecf9d04fc9b6f7ff07361715c71ef8703bf24c7905d4580808080808080a0cfa9df4025f175e0c4338efc950dc2e30214a08e8ba6940226a97fd977b156b08080808080808080,0xf9011130b9010d02f90109018301155cb9010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0 0x11ef2192f0c9aa092d69b9acf82085f384f172188cc321da94566dc9d33a3b18'
+  )}
+    Verify the 0xf851a02d247ca1770e3221e8aecf9d04fc9b6f7ff07361715c71ef8703bf24c7905d4580808080808080a0cfa9df4025f175e0c4338efc950dc2e30214a08e8ba6940226a97fd977b156b08080808080808080,0xf9011130b9010d02f90109018301155cb9010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0 merkle proof, with the 0x064eaad7fb35c25410aec7f55603d9bb95436891cbda68a4be9a6fc3b19ca0a5 transaction, and 0x11ef2192f0c9aa092d69b9acf82085f384f172188cc321da94566dc9d33a3b18 receipt trie root.
+
+  $ ${example(
+    'topos-zkevm-demo verify zk-proof /path/to/proofs/b00002.zkproof'
+  )}
+    Verify the zk-proof found at /path/to/proofs/b00002.zkproof.
+
+  $ ${example('topos-zkevm-demo stop')}
+    Stop the local-zkEVM infra. Your development chain can be resumed by running the start commmand again.
+
+  $ ${example('topos-zkevm-demo uninstall')}
+    Uninstall Topos-zkEVM-Demo. It will shut down all containers, and remove all filesystem artifacts except for log files.
 
   $ ${example('topos-zkevm-demo version')}
     This will print the version of the topos zkevm demo.
-
-  $ ${example('topos-zkevm-demo version -q')}
-    This will print only the numbers of the topos-zkevm-demo version, with no other output.
   
 Configuration
 
